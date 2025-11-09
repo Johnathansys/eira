47069a3d7703db89915becb1d585b87a789bc334
Johnathansys <lon24omir01@mpw.ac.uk>
2025-11-09 12:55:36 +0000

    app py sketch

--- app.py
+++ app.py
@@ -2,44 +2,83 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 import hashlib
 
-#initialising a database
-def init_db():
-    conn = sqlite3.connect('database.db')
-    cursor=conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS journal(
-                   id INTEGER PRIMARY KEY AUTOINCREMENT
-                   entry TEXT NOT NULL
-                   date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)'''
-    )
-    conn.commit()
-    conn.close()
 
-init_db()
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+app = Flask(__name__)
+app.secret_key = "random"
+con = sqlite3.connect("userdata.db")
+cur = con.cursor()
+cur.execute("""
+            CREATE TABLE IF NOT EXISTS User
+            (
+            username VARCHAR(20) NOT NULL PRIMARY KEY,
+            password VARCHAR(64) NOT NULL
+            )
+        """)
 
 
-@app.route('/resources')
-def resources():
-    return render_template('resources.html')
-
-@app.route('/journal', methods=['GET','POST'])
-def journal():
-    if request.method == 'POST':
-        entry = request.form['entry']
-        conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO journal (entry) VALUES (?)", (entry,))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('journal'))
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    else:
+        con = sqlite3.connect("userdata.db")
+        cur = con.cursor()
+        encoded= request.form["Password"].encode()
+        hash=hashlib.sha256(encoded).hexdigest()
+        print(hash)
+        cur.execute("INSERT INTO User (username, password) VALUES (?, ?)",
+                        (request.form["Username"], hash))
+        con.commit()
+        con.close()
+    return "Signup Sucessful"
     
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM journal ORDER BY date DESC")
-    entries = cursor.fetchall()
-    conn.close()
-    return render_template('journal.html', entries=entries)
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        con = sqlite3.connect("userdata.db")
+        cur = con.cursor()
+        hash= hashlib.sha256(request.form["Password"].encode()).hexdigest()
+        cur.execute("SELECT * FROM User WHERE username = ? and password = ?",
+                        (request.form["Username"], hash))
+        data = cur.fetchall()
+        con.commit()
+        con.close()
+        if len(data) == 0:
+            return "Login Unsucessful"
+        else:
+            session["Username"] = request.form["Username"]
+            return render_template("welcome.html")
 
+@app.route("/w")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/password")
+def password():
+    if request.method == "GET":    
+        if "Username" in session:
+            return render_template("change_password.html")
+        else:
+            return render_template("index.html")
+    else:
+        if "Username" in session:
+            con = sqlite3.connect("userdata.db")
+            cur = con.cursor()
+            hash= hashlib.sha256(request.form["Password"].encode()).hexdigest()
+            cur.execute("UPDATE User SET Password = ? WHERE Username = ?",
+                            (hash, session["Username"]))
+            con.commit()
+            con.close()
+            return "Password Updated"
+        else:
+            return render_template("index.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("Username", None)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
